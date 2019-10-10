0b6926dc0d7ed2ca18c550b8d3b2fe290251d9bc
haichungcn <haichungcn@gmail.com>
2019-10-10 11:23:36 +0700

    fix category

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let pageNum = 1;
 let newsArticles = [];
 let selectedSourceNames = [];
 let selectedCategoryNames = null;
-let sourceNames = [];
+let sourceNames = {};
 let categories = ['All', 'Business', 'Entertainment', 'General', 'Health', 'Science','Sports', 'Technology'];
 
 function stripHtml(html) {
@@ -30,12 +30,25 @@ if (window.clientInformation.language == "vi") {
 //     data.sources > 0 ? sourceNames = data.sources : console.log('There is no source fetched');
 // }
 
-async function fetchNews(country, category, topic) {
-    let url = `https://newsapi.org/v2/top-headlines?country=${country == null ? 'us' : country}${category == null ? '' : `&category=${category}`}${topic == null ? '' : `&topic=${topic}`}&apiKey=c8920a6401f9458eae1a7af498f324c4&page=${pageNum}`
+async function fetchNews(category) {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category == null ? 'general' : category}&apiKey=c8920a6401f9458eae1a7af498f324c4&page=${pageNum}`
     console.log(url);
     let response = await fetch(url);
     let data = await response.json();
-    data.totalResults > 0 ? newsArticles = newsArticles.concat(data.articles) : console.log('There is no news fetched');
+    newsArticles = data.articles;
+    console.log(newsArticles);
+    selectedSourceNames = [];
+    renderNews(newsArticles);
+    filterSources(newsArticles);
+    pageNum++;
+}
+
+async function fetchMoreNews(category) {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category == null ? 'general' : category}&apiKey=c8920a6401f9458eae1a7af498f324c4&page=${pageNum}`
+    console.log(url);
+    let response = await fetch(url);
+    let data = await response.json();
+    newsArticles = newsArticles.concat(data.articles);
     console.log(newsArticles);
     selectedSourceNames = [];
     renderNews(newsArticles);
@@ -45,6 +58,7 @@ async function fetchNews(country, category, topic) {
 
 function renderNews(articles) {
     let filteredArticles = articles.slice();
+    document.getElementById("renderNewsArea").innerHTML = "";
     if (selectedSourceNames.length > 0) {
         console.log("pass throughs electedSourceNames.length > 0")
         filteredArticles = articles.filter(article => selectedSourceNames.includes(article.source.name) && article)
@@ -78,17 +92,20 @@ function renderNews(articles) {
 
 const filterSources = (articles) => {
     articles.map(a => {
-        if(!sourceNames.includes(a.source.name)){
-            sourceNames.push(a.source.name)
+        let source = a.source.name;
+        if(sourceNames[source] == null){
+            sourceNames[source] = 0;
         };
+        sourceNames[source]++;
     })
-    displaySources(sourceNames);
+    displaySources();
 }
 
-const displaySources = (arr) => {
+const displaySources = () => {
+    let arr = Object.keys(sourceNames);
     const html = arr.map((sourceName, i) => {
         return `
-        <li class="list-group-item list-group-item-action pl-1 pl-sm-5"><input type="checkbox" class="form-check-input" id="${sourceName}" onchange="renderNewsBySource(${i})"><label for="${sourceName}">${sourceName.split('.')[0]}</label></li>
+        <li class="list-group-item list-group-item-action pl-1 pl-sm-5"><input type="checkbox" class="form-check-input" id="${sourceName}" onchange="renderNewsBySource(${i})"><label for="${sourceName}">${sourceName.split('.')[0]} : ${sourceNames[sourceName]}</label></li>
         `
     }).join('');
     document.getElementById("Sources").innerHTML = html;
@@ -96,15 +113,16 @@ const displaySources = (arr) => {
 
 const renderNewsBySource = (idx) => {
     let checkbox;
-    checkbox = document.getElementById(sourceNames[idx]);
+    let arr = Object.keys(sourceNames);
+    checkbox = document.getElementById(arr[idx]);
     if (checkbox.checked){
         sortBy = "source";
-        selectedSourceNames.push(sourceNames[idx]);
+        selectedSourceNames.push(arr[idx]);
         checkbox.parentElement.classList.add("active");
         // console.log('checkbox has been checked, running renderNews now, selectedSourceNames is: ', selectedSourceNames);
         renderNews(newsArticles);
     } else {
-        selectedSourceNames.map((val,i) => sourceNames[idx] == val && selectedSourceNames.splice(i,1));
+        selectedSourceNames.map((val,i) => arr[idx] == val && selectedSourceNames.splice(i,1));
         checkbox.parentElement.classList.remove("active");
         // console.log("checkbox id:", idx," has been unchecked, selectedSourceNames now is: ", selectedSourceNames);
         renderNews(newsArticles);
@@ -128,7 +146,7 @@ const generateCategoryDropdown = (categories) => {
 const handleCategoryDropdown = (cat) => {
     console.log('dropdown selected: ', cat);
     selectedSourceNames = [];
-    fetchNews('us', cat.toLowerCase());
+    fetchNews(cat.toLowerCase());
 }
 
 generateCategoryDropdown(categories);
